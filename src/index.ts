c24fb3781c1704e18cdba18d3d531717d977eb21
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-07-30 20:51:52 +0900

    feat: insert rows

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
-import { BigQuery } from '@google-cloud/bigquery';
+import {
+  BigQuery,
+  DatasetResponse,
+  TableResponse,
+} from '@google-cloud/bigquery';
 
 async function main() {
   const datasetId = 'bq_sandbox';
   const bq = new BigQuery();
 
-  const [dataset] = await bq.dataset(datasetId).get({ autoCreate: true });
+  const [dataset] = (await bq
+    .dataset(datasetId)
+    .get({ autoCreate: true })) as DatasetResponse;
   console.log(`Dataset ${dataset.id} created.`);
 
-  const [table] = await dataset
+  const [table] = (await dataset
     .table('bq_sandbox_table_01')
-    .get({ autoCreate: true });
+    .get({ autoCreate: true })) as TableResponse;
   console.log(`Table ${table.id} created.`);
+
+  await table.insert([
+    { name: 'John', age: 30 },
+    { name: 'Jane', age: 25 },
+  ]);
+  console.log(`Rows inserted.`);
 }
 
 main().catch((error) => {
