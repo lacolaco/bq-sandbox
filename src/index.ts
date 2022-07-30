79e8203e21319425347d498d6a69ebc8af41a562
Suguru Inatomi <suguru.inatomi@gmail.com>
2022-07-30 14:59:53 +0900

    feat: install bigquery SDK

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World');
+import { BigQuery } from '@google-cloud/bigquery';
+
+async function main() {
+  const datasetName = 'bq-sandbox';
+  const bq = new BigQuery();
+
+  const [dataset] = await bq.dataset(datasetName).get({ autoCreate: true });
+  console.log(`Dataset ${dataset.id} created.`);
+  console.log(dataset);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
